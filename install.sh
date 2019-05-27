198dcd7218cec4e34a48e025eac90d58204dd0cf
MintYiqingchen <821244608@qq.com>
2019-05-27 15:59:01 +0800

    update script

--- install.sh
+++ install.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 # pip install -r requirements.txt
-conda env create -f environment.yaml
+#conda env create -f environment.yaml
+conda create -n afteam_911 python=3.6
+
+# >>> conda init >>>
+
+__conda_setup="$(CONDA_REPORT_ERRORS=false '$HOME/anaconda3/bin/conda' shell.bash hook 2> /dev/null)"
+
+if [ $? -eq 0 ]; then
+
+    \eval "$__conda_setup"
+
+else
+
+    if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+
+        . "$HOME/anaconda3/etc/profile.d/conda.sh"
+
+        CONDA_CHANGEPS1=false conda activate base
+
+    else
+
+        \export PATH="$PATH:$HOME/anaconda3/bin"
+
+    fi
+
+fi
+
+unset __conda_setup
+
+# <<< conda init <<<
+
+conda activate afteam_911
+
+conda env update -f environment.yaml
--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
 
 # 根据项目，配置环境变量
-source activate afteam_911
+
+# >>> conda init >>>
+
+__conda_setup="$(CONDA_REPORT_ERRORS=false '$HOME/anaconda3/bin/conda' shell.bash hook 2> /dev/null)"
+
+if [ $? -eq 0 ]; then
+
+    \eval "$__conda_setup"
+
+else
+
+    if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+
+        . "$HOME/anaconda3/etc/profile.d/conda.sh"
+
+        CONDA_CHANGEPS1=false conda activate base
+
+    else
+
+        \export PATH="$PATH:$HOME/anaconda3/bin"
+
+    fi
+
+fi
+
+unset __conda_setup
+
+# <<< conda init <<<
+conda activate afteam_911
 export PRETRAINED_MODEL=./models/finalmodel.pth
 
 # 调用interface.py执行测试，其中data_rpath是数据集所在相对路径, team_number是参赛队伍编号，version_number是参赛队伍提交作品的版本号
-python interface.py --data_rpath $1 --team_number 911 --version_number 0.1
-
+python interface.py --data_rpath $1 --team_number 911 --version_number 0.2
