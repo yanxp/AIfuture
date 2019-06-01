e4c2c473b5b2c8886dac73ca65e7386612f8a96e
MintYiqingchen <821244608@qq.com>
2019-06-01 15:39:17 +0800

    add training crop script, test pass.

---
+++ retinaface/crop_aifuture.py
@@ -0,0 +1,75 @@
+from __future__ import print_function
+
+import argparse
+import sys
+import os
+import time
+import numpy as np
+import mxnet as mx
+from mxnet import ndarray as nd
+import cv2
+from .rcnn.logger import logger
+from .retinaface import RetinaFace
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Test widerface by retinaface detector')
+    # general
+    parser.add_argument('--network', help='network name', default='net3', type=str)
+    parser.add_argument('--data_rpath', help='dataset name', default='', type=str)
+    # testing
+    parser.add_argument('--prefix', help='model to test with', default='', type=str)
+    parser.add_argument('--epoch', help='model to test with', default=4, type=int)
+    parser.add_argument('--output', help='output folder', default='/home/chenriquan/aifuture/rf-cropped-training', type=str)
+    parser.add_argument('--nms', type=float, default=0.4)
+    parser.add_argument('--nocrop', action="store_true")
+    args = parser.parse_args()
+    return args
+
+detector = None
+args = None
+all_img_num = 0
+not_detected = 0
+os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT']='0'
+
+def test(args):
+  print('test with', args)
+  global detector
+  global all_img_num
+  global not_detected
+
+  output_root = os.path.join(args.output, 'images')
+  if not os.path.exists(output_root):
+    os.makedirs(output_root, exist_ok=True)
+  detector = RetinaFace(args.prefix, args.epoch, 0, args.network, args.nms, nocrop=args.nocrop, vote=True)
+
+  for cls in os.listdir(args.data_rpath):
+    for domain in ['0', '1']:
+      path = os.path.join(args.data_rpath, cls, domain)
+      for imgn in os.listdir(path):
+        imgp = os.path.join(path, imgn)
+        img = cv2.imread(imgp)
+        new_img = detector.get_input(img, threshold=0.02)
+
+        tmp = os.path.join(output_root, cls, domain)
+        os.makedirs(tmp, exist_ok=True)
+        tmp = os.path.join(tmp, imgn)
+        if new_img is None:
+          not_detected += 1
+          cv2.imwrite(tmp, img)
+        else:
+          cv2.imwrite(tmp, new_img)
+        print('save in ', tmp)
+        all_img_num += 1
+  
+  print('all_img_num: {}, not detected: {}, proportion: {:.3f}'.format(all_img_num, not_detected, not_detected/all_img_num))
+
+def main():
+    global args
+    args = parse_args()
+    logger.info('Called with argument: %s' % args)
+    test(args)
+
+if __name__ == '__main__':
+    main()
+
