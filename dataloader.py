3138de062ada5ec053cfd76806baf9893db940b6
yanxp <sysuyanxp@163.com>
2019-06-04 15:54:33 +0800

    cpp

--- dataloader.py
+++ dataloader.py
@@ -11,19 +11,20 @@ class TripletFace(data.Dataset):
         self.labels = []
         self.transforms = transform
         self.num_class = len(class_mapping)
-        for cls in os.listdir(root):
+        threshold = 6
+        for k ,cls in enumerate(os.listdir(root)):
             sample_cls = random.sample(os.listdir(root), 1)[0]
             while sample_cls== cls:
                 sample_cls = random.sample(os.listdir(root), 1)[0]
             photo = osp.join(root,cls,'1')
             comic = osp.join(root,cls,'0')
-            # sample_photo = osp.join(root,sample_cls,'1')
-            sample_comic = osp.join(root,sample_cls,'0')
-            threshold = 6
+            sample_photo = osp.join(root,sample_cls,'1')
+            #sample_comic = osp.join(root,sample_cls,'0')
             photo_tmp = os.listdir(photo)
             comic_tmp = os.listdir(comic)
             random.shuffle(photo_tmp)
             random.shuffle(comic_tmp)
+            """
             for anchor in photo_tmp[:threshold]:
                 for positive in comic_tmp[:threshold]:
                     negative = osp.join(sample_comic,random.sample(os.listdir(sample_comic),1)[0])
@@ -33,8 +34,19 @@ class TripletFace(data.Dataset):
                                         ))
                     positive_label = class_mapping[cls]
                     negative_label = class_mapping[sample_cls]
+                    self.labels.append((positive_label,negative_label)) 
+            """
+            for anchor in comic_tmp[:threshold]:
+                for positive in photo_tmp[:threshold]:
+                    negative = osp.join(sample_photo,random.sample(os.listdir(sample_photo),1)[0])
+                    self.triplets.append((osp.join(comic,anchor),
+                                         osp.join(photo,positive),
+                                         osp.join(sample_photo ,negative)
+                                          ))
+                    positive_label = class_mapping[cls]
+                    negative_label = class_mapping[sample_cls]
                     self.labels.append((positive_label,negative_label))
-
+    
     def __getitem__(self, index):
         anchor,positive,negative = self.triplets[index]
         label_p,label_n = self.labels[index]
